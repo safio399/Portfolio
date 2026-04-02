6379d421f69b215610cfd3601ba1d7542220e94a
safio399 <mrsafiopipo213@gmail.com>
2026-04-02 11:06:59 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/Portfolio">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
